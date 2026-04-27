4dc035be0fc55366dc13d03be816c2452b14d31e
Collin Brown <8021046+Collinbrown95@users.noreply.github.com>
2026-04-27 01:37:37 +0000

    add elementary index examples

--- core/src/elementary_indexes.rs
+++ core/src/elementary_indexes.rs
@@ -1,2 +1,20 @@
 use ndarray::ArrayView1;
 use crate::means::{arithmetic_mean, geometric_mean};
+
+pub fn carli_index(p1: ArrayView1<f64>, p0: ArrayView1<f64>) -> f64 {
+    let relatives = calculate_relatives(p1, p0);
+    arithmetic_mean(ArrayView1::from(&relatives))
+}
+
+pub fn jevons_index(p1: ArrayView1<f64>, p0: ArrayView1<f64>) -> f64 {
+    
+    let relatives = calculate_relatives(p1, p0);
+    geometric_mean(ArrayView1::from(&relatives))
+}
+
+fn calculate_relatives(p1: ArrayView1<f64>, p0: ArrayView1<f64>) -> Vec<f64> {
+    p1.iter()
+        .zip(p0.iter())
+        .map(|(a, b)| a / b)
+        .collect()
+}
--- python/src/lib.rs
+++ python/src/lib.rs
@@ -2,19 +2,25 @@ use pyo3::prelude::*;
 use numpy::PyReadonlyArray1;
 use core_logic::means::{arithmetic_mean, geometric_mean};
 
+// ==========================================================
+// MEANS MODULE
+// ==========================================================
+
 #[pyfunction]
-fn average(data: PyReadonlyArray1<f64>) -> f64 {
+#[pyo3(name = "arithmetic_mean")] // This is the name Python will see
+fn py_arithmetic_mean(data: PyReadonlyArray1<f64>) -> f64 {
     arithmetic_mean(data.as_array())
 }
 
 #[pyfunction]
-fn geo_mean(data: PyReadonlyArray1<f64>) -> f64 {
+#[pyo3(name = "geometric_mean")]
+fn py_geometric_mean(data: PyReadonlyArray1<f64>) -> f64 {
     geometric_mean(data.as_array())
 }
 
 #[pymodule]
-fn my_rust_lib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(average, m)?)?;
-    m.add_function(wrap_pyfunction!(geo_mean, m)?)?;
+fn means(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(py_arithmetic_mean, m)?)?;
+    m.add_function(wrap_pyfunction!(py_geometric_mean, m)?)?;
     Ok(())
 }
